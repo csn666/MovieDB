8913ef9778e4ebd40919cef00ce0c54288fcf2c0
csanzn <csanzn@uoc.edu>
2018-06-22 16:26:32 +0200

    commit

--- syn/src/androidTest/java/com/example/syn/ExampleInstrumentedTest.java
+++ syn/src/androidTest/java/com/example/syn/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.syn.test", appContext.getPackageName());
+        assertEquals("com.example.syn", appContext.getPackageName());
     }
 }
