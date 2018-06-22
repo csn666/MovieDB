b297114ff542e558d469bf901bc890b01a06223c
csanzn <csanzn@uoc.edu>
2018-06-22 17:15:27 +0200

    commit

--- app/src/main/java/com/example/csanz/moviedb/Syn/IServiceClient.java
+++ app/src/main/java/com/example/csanz/moviedb/Syn/IServiceClient.java
@@ -1,4 +1,15 @@
 package com.example.csanz.moviedb.Syn;
 
+import com.example.csanz.moviedb.Data.Movie;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface IServiceClient {
+    //Get list movies
+    @GET("list")
+    Call<List<Movie>> getListMovies(@Query("id") String id);
 }
